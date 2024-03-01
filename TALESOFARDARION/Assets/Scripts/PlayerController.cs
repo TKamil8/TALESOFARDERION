b72e8a5dadb287dbe5e2e4e0d99f37380f3c4b71
F1ejm <karolosrodka@gmail.com>
2024-03-01 20:35:43 +0100

    First NPC
    First NPC (rabbit) and interaction with it (press "e")  + small change with player script name

--- TALESOFARDARION/Assets/Scripts/PlayerController.cs
+++ TALESOFARDARION/Assets/Scripts/PlayerController.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+
     public Animator animator;
 
+    public LayerMask interactablesLayer;
+
     Vector2 movement;
+
+    public LayerMask solidObjectsLayer;
 
     // Update is called once per frame
     void Update()
@@ -20,10 +25,26 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            var facingDir = new Vector3(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"));
+            var interactPos = transform.position + facingDir;
+
+            Debug.DrawLine(transform.position, interactPos, Color.red, 0.4f);
+
+            var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactablesLayer);
+            if (collider != null)
+            {
+                Debug.Log("NPC here");
+            }
+        }
+    }
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+    
+   
+
 }
